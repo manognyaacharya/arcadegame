19fd547a606128ec0263e42d0237513105a9f51c
rohit9d <rohit9d@gmail.com>
2016-10-11 09:12:35 +0300

    created new box and checking collision

--- js/app.js
+++ js/app.js
@@ -33,17 +33,28 @@ Enemy.prototype.update = function(dt) {
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    drawBox(this.x, this.y + 77, 100, 67, "yellow");
 };
+//Enemy.prototype.detectCollision1 = function() {
+    //if (this.x < player.x + 101 && this.x + 101 > player.x && this.y < player.y + 65 && 65 + this.y > player.y){
+        //this.positionreset();
+        //console.log("collision");
+        //player.reset();
+    //}
+//};
 Enemy.prototype.detectCollision = function() {
-    if (this.x < player.x + 101 && this.x + 101 > player.x && this.y < player.y + 65 && 65 + this.y > player.y){
-        this.positionreset();
-        console.log("collision");
-        player.reset();
+    //console.log("inside collision function");
+    var enemyBox = {x: this.x, y: this.y + 77, width: 100, height:67};
+    var playerBox = {x: player.x + 16 , Y: player.y + 63 , width: 70, height: 75};
+    //console.log(enemyBox,playerBox);
+    if (enemyBox.x < playerBox.x + playerBox.width && enemyBox.x + enemyBox.width > playerBox.x && enemyBox.y < playerBox.y + playerBox.height && enemyBox.height + enemyBox.y > playerBox.y) {
+         this.positionreset();
+         console.log("collision");
+         player.reset();
     }
 };
 Enemy.prototype.positionreset = function() {
     this.x=0;
-    //player.reset();
 }
 
 // Now write your own player class
@@ -71,6 +82,7 @@ player.prototype.update = function(){
 
 player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite), this.x , this.y );
+    drawBox(this.x + 16,this.y + 63,70 , 75,"yellow" );
 };
 
 player.prototype.handleInput = function(key){
@@ -108,9 +120,16 @@ var bug1 = new Enemy(1,65);
 var bug2 = new Enemy(2,150);
 var bug3 = new Enemy(3,230);
 var allEnemies = [bug1,bug2,bug3];
-var player_initialX = 3*100;
+var player_initialX = 2*100;
 var player_initialY = 5*81;
 var player = new player(player_initialX, player_initialY);
+var drawBox = function(x,y,width,height,color){
+    ctx.beginPath();
+    ctx.rect(x, y, width, height);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = color;
+    ctx.stroke();
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
